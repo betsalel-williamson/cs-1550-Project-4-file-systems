da4bf775caac851357315bec152f2e04d40c1d9b
Betsalel Williamson <bhw7@pitt.edu>
2016-08-03 22:51:40 -0400

    Added tests to create file.

--- create_files.tcl
+++ create_files.tcl
@@ -46,44 +46,39 @@ proc create_files {directory} {
 
     cd "foo1"
 
-    if { [catch {set result [exec {*}[eval list {echo a > a.txt}]]} reason] } {
-
-    puts "Failed execution: $reason"
-
-    } else {
-
-    puts $result
-
+    set test_args {{echo "a" > 01.txt}
+        {echo "a" > 02.txt}
+        {echo "a" > 03.txt}
+        {echo "a" > 04.txt}
+        {echo "a" > 05.txt}
+        {echo "a" > 06.txt}
+        {echo "a" > 07.txt}
+        {echo "a" > 08.txt}
+        {echo "a" > 09.txt}
+        {echo "a" > 10.txt}
+        {echo "a" > 11.txt}
+        {echo "a" > 12.txt}
+        {echo "a" > 13.txt}
+        {echo "a" > 14.txt}
+        {echo "a" > 15.txt}
+        {echo "a" > 16.txt}
+        {echo "a" > 17.txt}
+        {echo "a" > 18.txt}
+        {ls}
     }
 
-    if { [catch {set result [exec {*}[eval list {ls}]]} reason] } {
+    foreach test $test_args {
+        puts "Executing: $test"
 
-    puts "Failed execution: $reason"
+        if { [catch {set result  [exec {*}[eval list $test]]} reason] } {
 
-    } else {
+        puts "Failed execution: $reason"
 
-    puts $result
+        } else {
 
-    }
+        puts $result
 
-    if { [catch {set result [exec {*}[eval list {echo b > b.txt}]]} reason] } {
-
-    puts "Failed execution: $reason"
-
-    } else {
-
-    puts $result
-
-    }
-
-    if { [catch {set result [exec {*}[eval list {ls}]]} reason] } {
-
-    puts "Failed execution: $reason"
-
-    } else {
-
-    puts $result
-
+        }
     }
 
     cd ".."
@@ -98,56 +93,20 @@ proc create_files {directory} {
 
     }
 
-    if { [catch {set result [exec {*}[eval list {ls}]]} reason] } {
-
-    puts "Failed execution: $reason"
-
-    } else {
-
-    puts $result
-
-    }
-
     cd "foo"
 
-    if { [catch {set result [exec {*}[eval list {echo "a" > a.txt}]]} reason] } {
+    foreach test $test_args {
+        puts "Executing: $test"
 
-    puts "Failed execution: $reason"
+        if { [catch {set result  [exec {*}[eval list $test]]} reason] } {
 
-    } else {
+        puts "Failed execution: $reason"
 
-    puts $result
+        } else {
 
-    }
+        puts $result
 
-    if { [catch {set result [exec {*}[eval list {ls}]]} reason] } {
-
-    puts "Failed execution: $reason"
-
-    } else {
-
-    puts $result
-
-    }
-
-    if { [catch {set result [exec {*}[eval list {echo "b" > b.txt}]]} reason] } {
-
-    puts "Failed execution: $reason"
-
-    } else {
-
-    puts $result
-
-    }
-
-    if { [catch {set result [exec {*}[eval list {ls}]]} reason] } {
-
-    puts "Failed execution: $reason"
-
-    } else {
-
-    puts $result
-
+        }
     }
 
     cd {..}
