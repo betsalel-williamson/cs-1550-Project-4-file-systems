edf06437be97590d42ee2478df6dd27bdc02d0c0
Betsalel Williamson <bhw7@pitt.edu>
2016-08-03 22:01:29 -0400

    Refactoring make directory test.

--- create_directories.tcl
+++ create_directories.tcl
@@ -23,17 +23,61 @@ proc create_directories {directory} {
     puts $result
 
     }
+# test creating one past max number of subdirectories
+    puts "\nExecuting: make max number of directories 'f01'\n"
 
-    set test_args {{mkdir "f"}
-        {mkdir "f0"}
-        {mkdir "f00"}
-        {mkdir "f000"}
-        {mkdir "f0000"}
-        {mkdir "f00000"}
-        {mkdir "f000000"}
-        {mkdir "f0000000"}
-        {mkdir "f0000000"}
-        {mkdir "f00000000"}
+    set test_args {{mkdir "f01"}
+        {mkdir "f02"}
+        {mkdir "f03"}
+        {mkdir "f04"}
+        {mkdir "f05"}
+        {mkdir "f06"}
+        {mkdir "f07"}
+        {mkdir "f08"}
+        {mkdir "f09"}
+        {mkdir "f10"}
+        {mkdir "f11"}
+        {mkdir "f12"}
+        {mkdir "f13"}
+        {mkdir "f14"}
+        {mkdir "f15"}
+        {mkdir "f16"}
+        {mkdir "f17"}
+        {mkdir "f18"}
+        {mkdir "f19"}
+        {mkdir "f20"}
+        {mkdir "f21"}
+        {mkdir "f22"}
+        {mkdir "f23"}
+        {mkdir "f24"}
+        {mkdir "f25"}
+        {mkdir "f26"}
+        {mkdir "f27"}
+        {mkdir "f28"}
+        {mkdir "f29"}
+        {mkdir "f30"}
+        {ls}
+    }
+
+    foreach test $test_args {
+        puts "Executing: $test"
+
+        if { [catch {set result  [exec {*}[eval list $test]]} reason] } {
+
+        puts "Failed execution: $reason"
+
+        } else {
+
+        puts $result
+
+        }
+    }
+
+# test making duplicate entry
+    puts "\nExecuting: make duplicate entry 'f01'\n"
+
+    set test_args {{mkdir "f01"}
+        {mkdir "f01"}
         {ls}
     }
 
